584d716f4b5f07bb1b42aa23417a8d4955b001cb
juliandashev <uliandashev09@gmail.com>
2025-01-30 19:36:15 +0200

    Improvements to the perft test solution

--- Perft_Tests/main.cpp
+++ Perft_Tests/main.cpp
@@ -1,8 +1,10 @@
+#define NOMINMAX
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <algorithm>
-#include <vector>>
+#include <vector>
 #include <iomanip>
 
 #include "../Engine/defs.h"
@@ -11,17 +13,17 @@ int main()
 {
 	std::string results_str{};
 
-	std::ifstream ml_results("../results.txt");
+	std::ifstream ce_results("../results.txt");
 	std::ifstream sf_results("../sf_results.txt");
 
 	std::vector<std::string> sf{};
-	std::vector<std::string> ml{};
+	std::vector<std::string> ce{};
 
-	ml.reserve(MAX_MOVES);
-	while (std::getline(ml_results, results_str))
-		ml.emplace_back(results_str);
+	ce.reserve(MAX_MOVES);
+	while (std::getline(ce_results, results_str))
+		ce.emplace_back(results_str);
 
-	ml_results.close();
+	ce_results.close();
 
 	sf.reserve(MAX_MOVES);
 	while (std::getline(sf_results, results_str))
@@ -29,26 +31,58 @@ int main()
 
 	sf_results.close();
 
-	std::sort(ml.begin(), ml.end());
+	std::sort(ce.begin(), ce.end());
 	std::sort(sf.begin(), sf.end());
 
-	const auto& bigger = ml.size() > sf.size() ? ml : sf;
-	const auto& smaller = ml.size() > sf.size() ? sf : ml;
+	const int ce_size = ce.size();
+	const int sf_size = sf.size();
 
-	std::cout << (bigger == ml ? "ce" : "sf")
-		<< "         " << (smaller == sf ? "sf" : "ml")
-		<< "\n" << std::string(25, '-') << std::endl;
+	const int total = std::max(ce_size, sf_size);
 
-	for (int i = 0; i < bigger.size(); i++)
+	std::cout << "Moves" << " | " << " sf     " << " | " << " ce" << "\n\n";
+
+	if (!std::abs(ce_size - sf_size)) // equal sizes
 	{
-		if (i < smaller.size())
+		for (int i = 0; i < ce_size; i++)
 		{
-			if (bigger[i] != smaller[i])
-				std::cout << bigger[i] << " | " << smaller[i] << std::endl;
+			std::string ce_move = ce[i].substr(0, 4);
+			std::string sf_move = sf[i].substr(0, 4);
+
+			if (ce_move == sf_move) // matching moves
+			{
+				std::string ce_nodes = ce[i].substr(6, ce[i].size());
+				std::string sf_nodes = sf[i].substr(6, sf[i].size());
+
+				if (ce_nodes != sf_nodes) // Print differences
+					std::cout << ce_move << "  |  " << sf_nodes << " |  " << ce_nodes << "\n";
+			}
+			else // not matching moves
+			{
+				// e1c1: 3551583
+
+				// check from next element, since current moves aren't equal
+				for (int j = i; j < ce_size; j++)
+				{
+					std::string sf_next_move = sf[j].substr(0, 4);
+
+					if (ce_move == sf_next_move) // if a match is found on the next moves
+					{
+						// std::cout << ce_move << "  |  " << sf_next_move << "\n";
+						
+						std::swap(ce[i], sf[j]);
+						break;
+					}
+				}
+
+				std::cout << sf[i].substr(0, 4) << "  |  " << ce[i].substr(0, 4) << "\n";
+			}
 		}
-		else
-			std::cout << bigger[i] << " | " << "----" << std::endl;
 	}
+	else // diff sizes
+	{
+
+	}
+
 
 	return 0;
 }
